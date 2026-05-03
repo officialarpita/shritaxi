10b6bdf6c0fc9fb31eda9129ef2cbbd2077405a3
officialarpita <officialarpita@gmail.com>
2026-05-04 01:29:51 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://shritaxi.in',
+  site: 'https://shritaxi.com',
   integrations: [sitemap()],
   build: {
     format: 'directory',
